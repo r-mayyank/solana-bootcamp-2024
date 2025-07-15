6eeb5977440010c039117f3a2e36de236f9b8da4
r_mayyank <thelueblue@gmail.com>
2025-07-15 23:05:59 +0530

    token-vesting init

---
+++ token-vesting/anchor/programs/counter/src/lib.rs
@@ -0,0 +1,48 @@
+#![allow(clippy::result_large_err)]
+
+use anchor_lang::prelude::*;
+use anchor_spl::token::{Mint};
+
+declare_id!("FqzkXZdwYjurnUKetJCAvaUw5WAqbwzU6gZEwydeEfqS");
+
+#[program]
+pub mod vesting {
+    use super::*;
+
+    pub fn create_vesting_account(ctx: Context<CreateVestingAccount>, company_name: String) -> Result<()> {
+
+        Ok(())
+    }
+}
+
+#[derive(Accounts)]
+#[instruction(company_name: String)]
+pub struct CreateVestingAccount<'info> {
+    #[account(mut)]
+    pub signer: Signer<'info>,
+
+    #[account(
+        init,
+        payer = signer,
+        space = 8 + VestingAccount::INIT_SPACE,
+        seeds = [company_name.as_ref()],
+        bump
+    )]
+    pub vesting_account: Box<Account<'info, VestingAccount>>,
+
+    pub mint: InterfaceAccount<'info, Mint>,
+
+    pub token_program: Program<'info, anchor_spl::token::Token>,
+}
+
+#[account]
+#[derive(InitSpace)]
+pub struct VestingAccount {
+    pub owner: Pubkey,
+    pub mint: Pubkey,
+    pub treasury_token_account: Pubkey,
+    #[max_len(50)]
+    pub company_name: String,
+    pub treasury_bump: u8,
+    pub bump: u8,
+}
